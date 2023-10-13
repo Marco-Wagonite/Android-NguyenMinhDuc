4b7eb94dac9090b391a0e6aa6f1e1c43e5d14f06
Marco <marcowagonite@gmail.com>
2023-10-13 20:01:50 +0700

    Update final project

---
+++ FlappyBird_Game/android/src/com/uef/flappy_bird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.uef.flappy_bird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.uef.flappy_bird.Flappy_Bird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Flappy_Bird(), config);
+	}
+}
